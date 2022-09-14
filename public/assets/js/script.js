13a69a6b49252fe705c9d616be0bd9a4d16e6574
neoxr <neoxr@null.net>
2022-09-14 13:33:33 +0700

    release

---
+++ public/assets/js/script.js
@@ -0,0 +1,42 @@
+const $ = el => {
+   return document.getElementById(el)
+}
+
+function upload() {
+   var file = $('files').files[0]
+   var formdata = new FormData()
+   formdata.append('files', file)
+   var ajax = new XMLHttpRequest()
+   ajax.upload.addEventListener('progress', progressHandler, false)
+   ajax.addEventListener('load', completeHandler, false)
+   ajax.addEventListener('error', errorHandler, false)
+   ajax.addEventListener('abort', abortHandler, false)
+   ajax.open('POST', '/upload')
+   ajax.send(formdata)
+}
+
+function progressHandler(event) {
+   var percent = (event.loaded / event.total) * 100
+   $('file-upload__label').innerHTML = 'Please wait ... ' + Math.round(percent) + '%'
+}
+
+function completeHandler(event) {
+   $('file-upload__label').innerHTML = event.target.responseText
+   setTimeout(function() {
+      $('file-upload__label').innerHTML = 'Select or drop files here'
+   }, 1500)
+}
+
+function errorHandler(event) {
+   $('file-upload__label').innerHTML = 'Something went wrong'
+   setTimeout(function() {
+      $('file-upload__label').innerHTML = 'Select or drop files here'
+   }, 1500)
+}
+
+function abortHandler(event) {
+   $('file-upload__label').innerHTML = 'Upload aborted'
+   setTimeout(function() {
+      $('file-upload__label').innerHTML = 'Select or drop files here'
+   }, 1500)
+}
